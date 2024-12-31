886f4db91701c0bbbdcd79afcb2730306c533351
Odingoii <okoromaloba@gmail.com>
2024-12-31 20:46:56 +0300

    seriously

--- api/backend/db.php
+++ api/backend/db.php
@@ -7,7 +7,7 @@
 $host = 'localhost';
 $user = 'portals';
 $password = 'I&Y*U&^(JN&Y Kjbkjn'; // Ensure this is the correct password
-$database = 'admin';
+$database = 'jkuatcu_admin';
 
 // Create connection
 $mysqli = new mysqli($host, $user, $password, $database);
