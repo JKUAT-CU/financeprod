e12b8f9c001ceec4968947d394640c4a25cb36bf
Odingoii <okoromaloba@gmail.com>
2025-01-02 22:33:29 +0300

    This is a joke

--- api/backend/db.php
+++ api/backend/db.php
@@ -1,11 +1,22 @@
 <?php
 // Database connection
 $host = 'localhost';
-$user = 'portals';
-$password = 'I&Y*U&^(JN&Y Kjbkjn'; // Ensure this is the correct password
-$database = 'admin';
+$user = 'jkuatcu_devs';
+$password = '#God@isAble!#';  // Ensure this is the correct password
+$database = 'jkuatcu_admin';
+
+// // Create connection
+// $mysqli = new mysqli($host, $user, $password, $database);
+
+// Database connection
+// $host = 'localhost';
+// $user = 'portals';
+// $password = 'I&Y*U&^(JN&Y Kjbkjn'; // Ensure this is the correct password
+// $database = 'admin';
 
 // Create connection
 $mysqli = new mysqli($host, $user, $password, $database);
+
+
 
 ?>
