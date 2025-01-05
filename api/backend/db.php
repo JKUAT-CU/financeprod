a63a7456ed1cb5718fea8e4dc1f904466a4ffd1c
Odingoii <okoromaloba@gmail.com>
2025-01-05 11:13:00 +0300

    Replace get_result with bind_result on fetch_budgets

--- api/backend/db.php
+++ api/backend/db.php
@@ -1,18 +1,18 @@
 <?php
 // Database connection
-// $host = 'localhost';
-// $user = 'jkuatcu_devs';
-// $password = '#God@isAble!#';  // Ensure this is the correct password
-// $database = 'jkuatcu_admin';
+$host = 'localhost';
+$user = 'jkuatcu_devs';
+$password = '#God@isAble!#';  // Ensure this is the correct password
+$database = 'jkuatcu_admin';
 
 // // Create connection
 // $mysqli = new mysqli($host, $user, $password, $database);
 
 // Database connection
-$host = 'localhost';
-$user = 'portals';
-$password = 'I&Y*U&^(JN&Y Kjbkjn'; // Ensure this is the correct password
-$database = 'admin';
+// $host = 'localhost';
+// $user = 'portals';
+// $password = 'I&Y*U&^(JN&Y Kjbkjn'; // Ensure this is the correct password
+// $database = 'admin';
 
 // Create connection
 $mysqli = new mysqli($host, $user, $password, $database);
